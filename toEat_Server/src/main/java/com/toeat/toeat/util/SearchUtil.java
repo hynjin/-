9d443c904d4d4073dce910b39bd382c9780ba3b9
hynjin <hynjin0110@gmail.com>
2020-08-03 22:10:58 +0900

    pom.xml

--- toEat_Server/src/main/java/com/toeat/toeat/util/SearchUtil.java
+++ toEat_Server/src/main/java/com/toeat/toeat/util/SearchUtil.java
@@ -7,17 +7,68 @@ import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.URLEncoder;
+
+import com.toeat.toeat.vo.ResponseVo;
 
 public class SearchUtil {
 	private static final Logger logger = LoggerFactory.getLogger(SearchUtil.class);
+
+	@Value("${kakao.restapi.key}")
+	private String restApiKey;
+    private static final String API_HOST  = "https://dapi.kakao.com";
+    private static final String SEARCH_PATH = "/v2/local/search/keyword.json";
 	
-	private List<String> category = new ArrayList<String>(Arrays.asList("디저트", "동남아음식", "인도음식", "양식", "한식", "중식", "일식", "분식", "패스트푸드", "뷔페"));
+	private static List<String> category = new ArrayList<String>(Arrays.asList("디저트", "동남아음식", "인도음식", "양식", "한식", "중식", "일식", "분식", "패스트푸드", "뷔페"));
 	
 	public List<String> getCategory(){
 		return category;
 	}
 	
+	public String getQuery() {
+		String query = "";
+		return query;
+	}
 	
+	public ResponseEntity<String> getSearchPlaceByKeyword() throws Exception {
+        String queryString = "?query="+"맛집"+"&page="+"3"+"&size="+"3";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Authorization", "KakaoAK " + restApiKey);
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
+
+        URI url = URI.create(API_HOST+SEARCH_PATH+queryString);
+        RequestEntity<String> request = new RequestEntity<>(headers, HttpMethod.GET, url);
+        ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+
+        return response;
+    }
+	/**
+	 * Remote URL (GET)
+	 * @param address
+	 * @return
+	 * @throws Exception
+	 */
+	public static ResponseVo getRequest(String address) throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(address, String.class);
+		HttpStatus status = responseEntity.getStatusCode();
+		if (status == HttpStatus.OK) {
+			return new ResponseVo("OK", responseEntity.getBody());
+		} else {
+			return new ResponseVo(status.toString(), responseEntity.getBody());
+		}
+	}
 	
 	public static int randInt(int min, int max) {
     	Random random = new Random(); // nextInt는 정상적인 경우 최대 값을 포함하지 않기 때문에, 1을 더함. 
